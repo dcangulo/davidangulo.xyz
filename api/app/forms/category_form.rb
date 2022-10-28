ddb6e4f11ce3119602ab1bfc6a9d27348722bca9
David Angulo <hello@davidangulo.xyz>
2022-10-28 20:19:17 +0800

    chore: repo cleanup

--- api/app/forms/category_form.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class CategoryForm < ApplicationForm
-  properties :name, :description, :slug, :parent_category_id
-
-  validates :name, presence: true
-end
--- api/app/forms/tag_form.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class TagForm < ApplicationForm
-  properties :name, :description, :slug
-
-  validates :name, presence: true
-end
