9e94ba743be05155606e43771e855c67e65aeb8c
David Angulo <hello@davidangulo.xyz>
2022-12-26 16:42:04 +0800

    chore: remove indented if

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
-# Check for changed posts
-
 Jekyll::Hooks.register :posts, :post_init do |post|
-  commit_num = `git rev-list --count HEAD "#{post.path}"`
+  commit_count = `git rev-list --count HEAD "#{post.path}"`.to_i
 
-  if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{post.path}"`
-    post.data['last_modified_at'] = lastmod_date
-  end
+  next if commit_count < 1
+
+  last_modified_date = `git log -1 --pretty="%ad" --date=iso "#{post.path}"`
+  post.data['last_modified_at'] = last_modified_date
 end
